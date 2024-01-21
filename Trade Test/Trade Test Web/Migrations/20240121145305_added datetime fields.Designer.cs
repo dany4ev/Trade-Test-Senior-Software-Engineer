1229f6ff601320e3ba2bb320499cb036bccd1511
dany4ev <10358572+dany4ev@users.noreply.github.com>
2024-01-21 19:53:41 +0500

    Added datetime field migrations

--- Trade Test/Trade Test Web/Migrations/20240121145305_added datetime fields.Designer.cs
+++ Trade Test/Trade Test Web/Migrations/20240121145305_added datetime fields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trade_Test.Data.EfModels;
 
@@ -11,9 +12,10 @@ using Trade_Test.Data.EfModels;
 namespace Trade_Test_Web.Migrations
 {
     [DbContext(typeof(TradeTestDbContext))]
-    partial class TradeTestDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240121145305_added datetime fields")]
+    partial class addeddatetimefields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Trade Test/Trade Test Web/Migrations/20240121145305_added datetime fields.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trade_Test_Web.Migrations
+{
+    public partial class addeddatetimefields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Vote",
+                table: "TblCharacters",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ModifiedDateTime",
+                table: "TblCharacters",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifiedDateTime",
+                table: "TblCharacters");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Vote",
+                table: "TblCharacters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
