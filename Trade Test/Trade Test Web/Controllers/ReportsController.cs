2be816d699612e62718895d24b0ee8f7cda42404
dany4ev <10358572+dany4ev@users.noreply.github.com>
2024-01-22 09:20:10 +0500

    Addition of report module in progress

---
+++ Trade Test/Trade Test Web/Controllers/ReportsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trade_Test_Web.Controllers {
+    public class ReportsController : Controller {
+        public IActionResult Index() {
+            ViewBag.PageName = "Reports";
+            return View();
+        }
+    }
+}
