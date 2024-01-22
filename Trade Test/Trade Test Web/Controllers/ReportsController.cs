41846eefb82cc4b08f91bb7df7ac6b10b8d37729
dany4ev <10358572+dany4ev@users.noreply.github.com>
2024-01-23 00:29:59 +0500

    Implemented reports

--- Trade Test/Trade Test Web/Controllers/ReportsController.cs
+++ Trade Test/Trade Test Web/Controllers/ReportsController.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using Trade_Test.Services.Interfaces;
+
 namespace Trade_Test_Web.Controllers {
     public class ReportsController : Controller {
+
+        private readonly ICharacterService _characterService;
+
+        public ReportsController(ICharacterService characterService) {
+            _characterService = characterService;
+        }
+
         public IActionResult Index() {
             ViewBag.PageName = "Reports";
-            return View();
+
+            var charactersList = _characterService.GetCharacters();
+
+            return View(charactersList);
         }
     }
 }
